b939bb7d1fce20992a0ac084635500073f42c6f9
4NDR32 <ademo18@gmail.com>
2022-09-11 16:41:18 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,88 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert */
+var gameBoardSize = 40;
+var gamePoints = 0;
+var gameSpeed = 100;
+
+$(document).ready(function () {
+  console.log("Ready Player One!");
+  createBoard();
+  $(".btn").click(function () {
+    startGame();
+  });
+});
+
+var Snake = {
+  position: [
+    [20, 20],
+    [20, 19],
+    [20, 18],
+  ], //snake start position
+  size: 3,
+  direction: "r",
+  alive: true,
+};
+
+var Food = {
+  position: [],
+  present: false,
+};
+
+function createBoard() {
+  $("#gameBoard").empty();
+  var size = gameBoardSize;
+
+  for (i = 0; i < size; i++) {
+    $("#gameBoard").append('<div class="row"></div>');
+    for (j = 0; j < size; j++) {
+      $(".row:last-child").append('<div class="pixel"></div>');
+    }
+  }
+}
+
+function moveSnake() {
+  var head = Snake.position[0].slice();
+
+  switch (Snake.direction) {
+    case "r":
+      head[1] += 1;
+      break;
+    case "l":
+      head[1] -= 1;
+      break;
+    case "u":
+      head[0] -= 1;
+      break;
+    case "d":
+      head[0] += 1;
+      break;
+  }
+
+  //check if head is moved
+  if (alive(head)) {
+    //draw head
+    $(
+      ".row:nth-child(" + head[0] + ") > .pixel:nth-child(" + head[1] + ")"
+    ).addClass("snakePixel");
+
+    //draw rest of the body loop
+    for (var i = 0; i < Snake.size; i++) {
+      $(
+        ".row:nth-child(" +
+          Snake.position[i][0] +
+          ") > .pixel:nth-child(" +
+          Snake.position[i][1] +
+          ")"
+      ).addClass("snakePixel");
+    }
+    //if head touches the food
+    if (
+      head.every(function (e, i) {
+        return e === Food.position[i];
+      })
+    ) {
+      Snake.size++;
+      Food.present = false;
+    }
+  }
+}
